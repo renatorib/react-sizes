83b6b44fd3c7a489602ea0c7876eb07ee5b0a870
Renato Ribeiro <renatoribroman@gmail.com>
2017-06-08 23:39:29 -0300

    Pass sizes on initial render - fixes #7 (#8)
    * Pass size props on initial render - my solution to #7. Also add .editorconfig and use react-display-name package instead of local implementation
    
    * Fix `window` undefined during SSR

--- src/utils/get-display-name.js
+++
@@ -1,8 +0,0 @@
-const getDisplayName = Component => {
-  if (typeof Component === 'string') return Component;
-  if (!Component) return undefined;
-
-  return Component.displayName || Component.name || 'Component';
-}
-
-export default getDisplayName;
