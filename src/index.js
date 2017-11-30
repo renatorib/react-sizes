878d308e7172df87aff28a138bb04ef0671b2818
Renato Ribeiro <renatoribroman@gmail.com>
2017-11-30 18:57:29 -0200

    fix lint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import withSizes from './withSizes'
 import * as presets from './presets'
 
-/* 
-// this will be a breaking change
-export createSizedComponent from './createSizedComponent'
-
-const all = Object.assign({}, exports)
-delete all.default */
-
 export default Object.assign(withSizes, presets)
+
