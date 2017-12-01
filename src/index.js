8e455121bfeebe0c494e3c46ece74a446fbc5d4d
Renato Ribeiro <renatoribroman@gmail.com>
2017-11-30 23:02:07 -0200

    fix presets import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import withSizes from './withSizes'
 import createSizedComponent from './createSizedComponent'
-import presets from './presets'
+import * as presets from './presets'
 
 export { createSizedComponent, presets, withSizes }
 export default withSizes
